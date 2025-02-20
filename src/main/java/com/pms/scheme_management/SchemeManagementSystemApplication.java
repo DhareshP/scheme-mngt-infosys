65259c54c3fc2dd39a716c046e2c8123887949d3
DhareshP <dharesh.a.p@gmail.com>
2025-02-20 18:51:40 +0530

    scheme management

---
+++ src/main/java/com/pms/scheme_management/SchemeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pms.scheme_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pms/scheme_management/SchemeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pms.scheme_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
