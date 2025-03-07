0a9615127cb85d52af4a9ad678bb26585e5084de
DhareshP <dharesh.a.p@gmail.com>
2025-03-07 21:11:22 +0530

    smtp

--- src/main/java/com/pms/scheme_management/service/UserSchemeService.java
+++ src/main/java/com/pms/scheme_management/service/UserSchemeService.java
@@ -1,5 +1,6 @@
 package com.pms.scheme_management.service;
 
+import com.pms.scheme_management.exception.SchemeNotFoundException;
 import com.pms.scheme_management.model.Scheme;
 import com.pms.scheme_management.model.UserSchemes;
 import com.pms.scheme_management.repository.SchemeRepository;
@@ -18,15 +19,26 @@ public class UserSchemeService {
     @Autowired
     UserSchemesRepository userSchemesRepository;
 
+    @Autowired
+    EmailService emailService;
+
     public UserSchemes applyToScheme(int schemeId, int userId){
-        Scheme scheme = schemeRepository.findById(schemeId).orElseThrow(() -> new RuntimeException("Scheme not found with id: "+ schemeId));
+        Scheme scheme = schemeRepository.findById(schemeId).orElseThrow(() -> new SchemeNotFoundException("Scheme not found with id: "+ schemeId));
 
         if (!scheme.isSchemeIsActive()) {
-            throw new RuntimeException("Scheme not active for application");
+            throw new SchemeNotFoundException("Scheme not active for application");
         }
 
         UserSchemes us = new UserSchemes(scheme, userId);
-        return userSchemesRepository.save(us);
+        UserSchemes savedUs = userSchemesRepository.save(us);
+
+        String email = "dhareshcoc@gmail.com"; // Fetch user email from pms db
+        String subject = "Scheme Application Confirmation";
+        String body = "Dear User,<br><br>You have successfully applied for the scheme: <b>"
+                + scheme.getSchemeName() + "</b>.<br>Thank you!<br><br>Regards,<br>Scheme Management Team";
+
+        emailService.sendEmail(email , subject , body);
+        return savedUs;
     }
 
     public List<UserSchemes> allAppliedSchemesOfUser(int userId){
